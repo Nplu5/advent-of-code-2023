954df1a884fab842127b05063823ada5eed1d766
Nplu5 <simon+github@skrause.me>
2023-12-05 16:05:04 +0100

    Add Parsing logic

--- src/main/kotlin/org/skrause/five/Day05.kt
+++ src/main/kotlin/org/skrause/five/Day05.kt
@@ -9,9 +9,73 @@ fun main() {
 }
 
 fun solvePart1(input: List<String>): Int {
+    val file = parsePuzzleFile(input)
     return 1
 }
 
 fun solvePart2(input: List<String>): Int {
     return 1
+}
+
+val orderMap = mapOf(
+    "seed-to-soil" to 1,
+    "soil-to-fertilizer" to 2,
+    "fertilizer-to-water" to 3,
+    "water-to-light" to 4,
+    "light-to-temperature" to 5,
+    "temperature-to-humidity" to 6,
+    "humidity-to-location" to 7
+)
+
+fun parsePuzzleFile(input: List<String>): PuzzleFile {
+    val seeds = extractSeeds(input[0])
+    val mappingCollections = input.slice(IntRange(2, input.size - 1))
+        .joinToString("|")
+        .split("||")
+        .map { it.split("|").joinToString("\n") }
+        .map(MappingCollection::parse)
+        .sortedWith(MappingComparator())
+    return PuzzleFile(seeds, mappingCollections)
+}
+
+class MappingComparator : Comparator<MappingCollection> {
+    override fun compare(o1: MappingCollection?, o2: MappingCollection?): Int {
+        require(o1 != null) { "Mapping collection cannot be null" }
+        require(o2 != null) { "Mapping collection cannot be null" }
+        require(orderMap[o1.name] != null) { "Unknown mapping: ${o1.name}" }
+        require(orderMap[o2.name] != null) { "Unknown mapping: ${o2.name}" }
+        return orderMap[o1.name]!! - orderMap[o2.name]!!
+    }
+}
+
+data class PuzzleFile(val seeds: List<Seed>, val mappingCollections: List<MappingCollection>)
+
+typealias Seed = Int
+
+fun extractSeeds(seedLine: String): List<Seed> = seedLine.split(": ")[1]
+    .split(" ")
+    .map { it.toInt() }
+
+data class Mapping(val destinationStart: Int, val sourceStart: Int, val numberOfSteps: Int) {
+    companion object {
+        fun parse(textToParse: String): Mapping {
+            val (destination, source, range) = textToParse.split(" ")
+                .map(String::toInt)
+            return Mapping(destination, source, range)
+        }
+    }
+}
+
+data class MappingCollection(val name: String, val mappings: List<Mapping>) {
+    companion object {
+        fun parse(textToParse: String): MappingCollection {
+            val lines = textToParse.split(("\n"))
+            val name = lines.first()
+                .split(" ")
+                .first()
+            val mappings = lines.slice(IntRange(1, lines.size - 1))
+                .map(Mapping::parse)
+            return MappingCollection(name, mappings)
+        }
+    }
 }
--- src/test/kotlin/org/skrause/five/Day05Test.kt
+++ src/test/kotlin/org/skrause/five/Day05Test.kt
@@ -4,11 +4,88 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.skrause.utils.readInput
 import strikt.api.expectThat
+import strikt.assertions.contains
+import strikt.assertions.first
 import strikt.assertions.isEqualTo
 import kotlin.test.Test
 
 @DisplayName("Day 5")
 class Day05Test {
+
+    @Test
+    @DisplayName("Parse Seeds")
+    fun parseSeeds() {
+        // Arrange
+        val input = "seeds: 79 14 55 13"
+
+        // Act
+        val seeds = extractSeeds(input)
+
+        // Assert
+        expectThat(seeds).contains(listOf(79, 14, 13, 55))
+    }
+
+    @Test
+    @DisplayName("Parse a Mapping")
+    fun parseMapping() {
+        // Arrange
+        val input = "50 98 2"
+        val expectedMapping = Mapping(50, 98, 2)
+
+        // Act
+        val mapping = Mapping.parse(input)
+
+        // Assert
+        expectThat(mapping).isEqualTo(expectedMapping)
+    }
+
+    @Test
+    @DisplayName("Can parse a mapping collection")
+    fun parseMappingCollection() {
+        // Arrange
+        val input = """
+            seed-to-soil map:
+            50 98 2
+            52 50 48
+        """.trimIndent()
+        val expectedMappingCollection = MappingCollection(
+            "seed-to-soil",
+            listOf(
+                Mapping(50, 98, 2),
+                Mapping(52, 50, 48)
+            )
+        )
+
+        // Act
+        val mappingCollection = MappingCollection.parse(input)
+
+        // Assert
+        expectThat(mappingCollection).isEqualTo(expectedMappingCollection)
+    }
+
+    @Test
+    @DisplayName("Parse puzzle file")
+    fun canParsePuzzleFile() {
+        // Arrange
+        val input = readInput("Day05/part01-test")
+
+        // Act
+        val result = parsePuzzleFile(input)
+
+        // Assert
+        expectThat(result.mappingCollections) {
+            first().isEqualTo(
+                MappingCollection(
+                    "seed-to-soil",
+                    listOf(
+                        Mapping(50, 98, 2),
+                        Mapping(52, 50, 48)
+                    )
+                )
+            )
+        }
+    }
+
     @Nested
     @DisplayName("Part 1")
     inner class Part1Test {
